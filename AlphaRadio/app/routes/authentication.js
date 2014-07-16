cee077d05f22c4800744ef84a2079e7fa944dea4
Thiago Dutra Chiarato <thiago.chiarato@hotmail.com>
2014-07-16 16:15:21 -0300

    removed

--- AlphaRadio/app/routes/authentication.js
+++
@@ -1,8 +0,0 @@
-var Router     = require('express').Router,
-    auth       = Router(),
-    controller = require('../controllers/authentication');
-
-auth.route('/')
-    .post(controller.signin);
-
-module.exports = auth;
--- AlphaRadio/config/config.js
+++
@@ -1,6 +0,0 @@
-var _ = require('underscore');
-
-// Load app configuration
-
-module.exports = _.extend(
-    require(__dirname + '/env/' + (process.env.NODE_ENV || 'development') + '.json') || {});
